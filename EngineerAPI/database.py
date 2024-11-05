e8d3964ee1bfbc8a51058390d5a8dace9188eb5a
Dylan Nicolay <108036935+Nicolax0@users.noreply.github.com>
2024-11-05 01:27:48 -0500

    Fix database connection issue

--- EngineerAPI/database.py
+++ EngineerAPI/database.py
@@ -1,22 +1,19 @@
 import psycopg2
-from psycopg2 import sql
 import os
 
 def connect_to_db():
     try:
         # Establish the connection
         conn = psycopg2.connect(
-            host=os.getenv("POSTGRES_HOST"),
             database=os.getenv("POSTGRES_DB"),
             user=os.getenv("POSTGRES_USER"),
             password=os.getenv("POSTGRES_PASSWORD"),
+            host=os.getenv("PG_HOST"),
             port=os.getenv("POSTGRES_PORT")
         )
+        print(f"{os.getenv('POSTGRES_DB')} database connected") # Print the database name
+        cursor = conn.cursor()
         print("Connection to PostgreSQL DB successful")
-        return conn
     except Exception as e:
         print(f"Error connecting to PostgreSQL DB: {e}")
-        return None
-
-# Example usage
-# connection = connect_to_db(host='localhost', database='mydb', user='myuser', password='mypassword')
+        return None, None
