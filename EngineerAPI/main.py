43d5a9e10b874fec830411aa4b4825e59772c146
lgbaccay <baccal@rpi.edu>
2024-11-15 17:21:16 -0500

    Remove duplicate on_ready

--- EngineerAPI/main.py
+++ EngineerAPI/main.py
@@ -18,10 +18,6 @@ intents.message_content = True  # Necessary for reading message content
 # Create an instance of Bot with a command prefix
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-@bot.event
-async def on_ready():
-    print(f'Logged on as {bot.user}')
-
 @bot.command(name='ping')
 async def ping(ctx):
     if ctx.channel.id == CHANNEL:
