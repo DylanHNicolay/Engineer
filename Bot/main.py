735b7eb47b810deef2a0da95ec8a2d5d0c157c6b
Dylan Nicolay <108036935+Nicolax0@users.noreply.github.com>
2025-08-26 16:26:53 -0400

    Revert changes (for real this time)

--- Bot/main.py
+++ Bot/main.py
@@ -25,7 +25,7 @@ class EngineerBot(commands.Bot):
             user=os.getenv('POSTGRES_USER'),
             password=os.getenv('POSTGRES_PASSWORD'),
             database=os.getenv('POSTGRES_DB'),
-            host='postgres'
+            host=os.getenv('POSTGRES_HOST')
         )
         self.db_interface = DatabaseInterface(pool)
         
