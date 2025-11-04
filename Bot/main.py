d370523c5c388e9d2989bee30baa14460ef59432
Dylan Nicolay <108036935+Nicolax0@users.noreply.github.com>
2025-11-04 16:30:30 -0500

    Disable allowed mentions due to using only slash commands

--- Bot/main.py
+++ Bot/main.py
@@ -13,18 +13,21 @@ intents = discord.Intents.all()
 
 class MyClient(commands.Bot):
     def __init__(self, *, intents: discord.Intents):
-        super().__init__(command_prefix='', intents=intents)
+        super().__init__(command_prefix='', intents=intents, allowed_mentions=None)
+        self._synced = False
 
     async def setup_hook(self):
         await db.connect()
         await self.load_extension("Teams.teams")
         await self.load_extension("Admin.admin")
 
-        guild = discord.Object(id=1281629365939208233)
-        self.tree.clear_commands(guild=guild)
-        await self.tree.sync(guild=guild)
-
     async def on_ready(self):
+        if not self._synced:
+            guild = discord.Object(id=1281629365939208233)
+            self.tree.clear_commands(guild=guild)
+            await self.tree.sync(guild=guild)
+            self._synced = True
+
         print(f'Logged in as {self.user} (ID: {self.user.id})') # type: ignore
         print('------')
 
