0a83869311eb26e48e5426a5f937aa876132e153
TheManaDork <zeldagamer36@gmail.com>
2026-03-12 17:01:43 -0400

    removed unfinished/unnecessary files

--- Bot/Webscrape/dialogue.py
+++
@@ -1,7 +0,0 @@
-
-
-class View(discord.ui.View):
-    @discord.ui.button(label="Quit", style=discord.ButtonStyle.red)
-    async def button_callback(self, button, interaction):
-        await button.response.send_message("Ending session.")
-        driver.quit()
